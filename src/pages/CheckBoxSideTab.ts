c43aa51b050100e985ca847f2a9956bddc0f8e0a
Sadab Khan <mdsadabkhan@gmail.com>
2025-04-26 20:28:28 +0530

    page update for checkbox

--- src/pages/CheckBoxSideTab.ts
+++ src/pages/CheckBoxSideTab.ts
@@ -1,4 +1,5 @@
 import Utils from "../../app_common/utils/Utils";
+import { expect, type Page } from "@playwright/test";
 
 class CheckBoxSideTab extends Utils{
     expandAllOption:string
@@ -30,31 +31,31 @@ class CheckBoxSideTab extends Utils{
     constructor(page:any){
         super(page)
         this.page=page;
-        this.expandAllOption='button.rct-option-expand-all'
-        this.collapseAllOption='button[aria-label="Collapse all"]'
+        this.expandAllOption='button.rct-option-expand-all' //button[title="Expand all"]
+        this.collapseAllOption='button[aria-label="Collapse all"]'  //button[title="Collapse all"]
         this.expandHomeOption='button.rct-collapse-btn'
-        this.expandDesktopOption='button.rct-collapse:nth-of-type(1)'
-        this.expandDocumentOption=''
-        this.expandDownloadoption=''
+        this.expandDesktopOption='button.rct-collapse:nth-of-type(1)'  
+        this.expandDocumentOption='label[for="tree-node-documents"]' // here onwards is AI locators
+        this.expandDownloadoption='label[for="tree-node-downloads"]'
         this.expandWorkSpaceOption=''
         this.expandOfficeOption=''
-        this.notesCheckbox=''
-        this.homeCheckbox=''
-        this.desktopCheckbox=''
-        this.commandsCheckbox=''
+        this.notesCheckbox='label[for="tree-node-notes"] input[type="checkbox"]'
+        this.homeCheckbox='label[for="tree-node-home"]'
+        this.desktopCheckbox='label[for="tree-node-desktop"]'
+        this.commandsCheckbox='label[for="tree-node-commands"] input[type="checkbox"]'
         this.documentsCheckbox=''
-        this.workspaceCheckbox=''
-        this.reactCheckbox=''
-        this.angularCheckBox=''
-        this.veuCheckbox=''
-        this.officeCheckbox=''
-        this.pubilcCheckbox=''
-        this.privateCheckbox=''
-        this.classifiedCheckbox=''
-        this.generalCheckbox=''
-        this.downloadsCheckBox=''
-        this.wordfiledocCheckbox=''
-        this.excelFileDocCheckbox=''
+        this.workspaceCheckbox='label[for="tree-node-workspace"] input[type="checkbox"]'
+        this.reactCheckbox='label[for="tree-node-react"] input[type="checkbox"]'
+        this.angularCheckBox='label[for="tree-node-angular"] input[type="checkbox"]'
+        this.veuCheckbox='label[for="tree-node-veu"] input[type="checkbox"]'
+        this.officeCheckbox='label[for="tree-node-office"] input[type="checkbox"]'
+        this.pubilcCheckbox='label[for="tree-node-public"] input[type="checkbox"]'
+        this.privateCheckbox='label[for="tree-node-private"] input[type="checkbox"]'
+        this.classifiedCheckbox='label[for="tree-node-classified"] input[type="checkbox"]'
+        this.generalCheckbox='label[for="tree-node-general"] input[type="checkbox"]'
+        this.downloadsCheckBox='label[for="tree-node-downloads"] input[type="checkbox"]'
+        this.wordfiledocCheckbox='label[for="tree-node-wordFile"] input[type="checkbox"]'
+        this.excelFileDocCheckbox='label[for="tree-node-excelFile"] input[type="checkbox"]'
     }
     async expandAllSectionAtOnce(){
         await this.clickOn(this.expandAllOption)
